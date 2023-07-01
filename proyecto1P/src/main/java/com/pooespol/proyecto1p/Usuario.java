cdf07a80bc02b53223b31e7b62937457080e98d1
JortanRGarcia <jorgsant@espol.edu.ec>
2023-07-01 12:08:15 -0500

    creacion user

--- proyecto1P/src/main/java/com/pooespol/proyecto1p/Usuario.java
+++ proyecto1P/src/main/java/com/pooespol/proyecto1p/Usuario.java
@@ -10,5 +10,70 @@ package com.pooespol.proyecto1p;
  * @author jorgs
  */
 public class Usuario {
+    protected int id;
+    protected String nombres;
+    protected String apellidos;
+    protected String correo;
+    protected String clave;
+    protected String organizacion;
+
+    public Usuario(int id, String nombres, String apellidos, String correo, String clave, String organizacion) {
+        this.id = id;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.clave = clave;
+        this.organizacion = organizacion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getOrganizacion() {
+        return organizacion;
+    }
+
+    public void setOrganizacion(String organizacion) {
+        this.organizacion = organizacion;
+    }
+    
+    
     
 }
