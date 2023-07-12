187f78f6d509a8268d9924d0eaa14a92a50942f4
Kcmoranj <138317440+Kcmoranj@users.noreply.github.com>
2023-07-11 21:15:58 -0500

    Update Vendedor.java

--- proyecto1P/src/main/java/com/pooespol/proyecto1p/Vendedor.java
+++ proyecto1P/src/main/java/com/pooespol/proyecto1p/Vendedor.java
@@ -2,3 +2,9 @@ Public class Vendedor extends Usuario {
   private ArrayList<Vehiculo> vehiculos;
 
   public Vendedor(int id,String nombres,String apellidos, String clave,String organizacion)){
+  super(id,nombres,apellidos,correo,clave,organizacion);
+    this,vehiculos=new Arraylist<>();
+  }
+  public Arraylist<Vehiculo> getVehiculos(){
+    return vehiculos;
+  }
