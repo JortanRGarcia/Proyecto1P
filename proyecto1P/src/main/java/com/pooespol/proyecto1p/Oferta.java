7ad10492197b6a9d561a9194fa8fd8454e3e9c65
Niariasve <138833714+Niariasve@users.noreply.github.com>
2023-07-11 22:20:24 -0500

    Update Oferta.java

--- proyecto1P/src/main/java/com/pooespol/proyecto1p/Oferta.java
+++ proyecto1P/src/main/java/com/pooespol/proyecto1p/Oferta.java
@@ -1,68 +1,68 @@
 package com.pooespol.proyecto1p;
 
 public class Oferta {
-  private int id;
-  private int idComprador;
-  private double valor;
-  private Comprador comprador;
-  private Vehiculo vehiculo;
-  private int idVehiculo;
+    private int id;
+    private int idComprador;
+    private double valor;
+    private Comprador comprador;
+    private Vehiculo vehiculo;
+    private int idVehiculo;
 
-  public Oferta(int id, int idComprador, double valor, Comprador comprador, Vehiculo vehiculo, int idVehiculo) {
-    this.id = id;
-    this.idComprador = idComprador;
-    this.valor = valor;
-    this.comprador = comprador;
-    this.vehiculo = vehiculo;
-    this.idVehiculo = idVehiculo;
-  }
+    public Oferta(int id, int idComprador, double valor, Comprador comprador, Vehiculo vehiculo, int idVehiculo) {
+        this.id = id;
+        this.idComprador = idComprador;
+        this.valor = valor;
+        this.comprador = comprador;
+        this.vehiculo = vehiculo;
+        this.idVehiculo = idVehiculo;
+    }
 
-  public int getId() {
-    return this.id;
-  }
+    public int getId() {
+        return this.id;
+    }
 
-  public void setId(int id) {
-    this.id = id;
-  }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-  public int getIdComprador() {
-    return this.idComprador;
-  }
+    public int getIdComprador() {
+        return this.idComprador;
+    }
 
-  public void setIdComprador(int idComprador) {
-    this.idComprador = idComprador;
-  }
+    public void setIdComprador(int idComprador) {
+        this.idComprador = idComprador;
+    }
 
-  public double getValor() {
-    return this.valor;
-  }
+    public double getValor() {
+        return this.valor;
+    }
 
-  public void setValor(double valor) {
-    this.valor = valor;
-  }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
 
-  public Comprador getComprador() {
-    return this.comprador;
-  }
+    public Comprador getComprador() {
+        return this.comprador;
+    }
 
-  public void setComprador(Comprador comprador) {
-    this.comprador = comprador;
-  }
+    public void setComprador(Comprador comprador) {
+        this.comprador = comprador;
+    }
 
-  public Vehiculo getVehiculo() {
-    return this.vehiculo;
-  }
+    public Vehiculo getVehiculo() {
+        return this.vehiculo;
+    }
 
-  public void setVehiculo(Vehiculo vehiculo) {
-    this.vehiculo = vehiculo;
-  }
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
 
-  public int getIdVehiculo() {
-    return this.idVehiculo;
-  }
+    public int getIdVehiculo() {
+        return this.idVehiculo;
+    }
 
-  public void setIdVehiculo(int idVehiculo) {
-    this.idVehiculo = idVehiculo;
-  }
-
+    public void setIdVehiculo(int idVehiculo) {
+        this.idVehiculo = idVehiculo;
+    }
+}
 
