a7b539149adc9abf89cd7b81d78421db9bc846ee
Kcmoranj <138317440+Kcmoranj@users.noreply.github.com>
2023-07-11 22:49:40 -0500

    Update Vendedor.java

--- proyecto1P/src/main/java/com/pooespol/proyecto1p/Vendedor.java
+++ proyecto1P/src/main/java/com/pooespol/proyecto1p/Vendedor.java
@@ -1,10 +1,77 @@
-Public class Vendedor extends Usuario {
-  private ArrayList<Vehiculo> vehiculos;
 
-  public Vendedor(int id,String nombres,String apellidos, String clave,String organizacion)){
-  super(id,nombres,apellidos,correo,clave,organizacion);
-    this,vehiculos=new Arraylist<>();
-  }
-  public Arraylist<Vehiculo> getVehiculos(){
-    return vehiculos;
-  }
+public class Vendedor extends Usuario {
+    private List<Vehiculo> vehiculos;
+
+    public Vendedor(int id, String nombres, String apellidos, String correo, String clave, String organizacion) {
+        super(id, nombres, apellidos, correo, clave, organizacion);
+        this.vehiculos = new ArrayList<>();
+    }
+
+    public List<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
+
+    public void setVehiculos(List<Vehiculo> vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+
+    public void registrarVendedor() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese los siguientes datos:");
+        System.out.print("Nombres: ");
+        String nombres = scanner.nextLine();
+        System.out.print("Apellidos: ");
+        String apellidos = scanner.nextLine();
+        System.out.print("Organización: ");
+        String organizacion = scanner.nextLine();
+        System.out.print("Correo Electrónico: ");
+        String correo = scanner.nextLine();
+
+        if (existeCorreo(correo)) {
+            System.out.println("El correo ya está registrado.");
+            return;
+        }
+
+        System.out.print("Clave: ");
+        String clave = scanner.nextLine();
+        String hashClave = generarHash(clave);
+        int nuevoId = obtenerNuevoId();
+        Vendedor nuevoVendedor = new Vendedor(nuevoId, nombres, apellidos, correo, hashClave, organizacion);
+        ListaVendedores.agregarVendedor(nuevoVendedor);
+
+        System.out.println("Registro exitoso.");
+    }
+
+    public void ingresarNuevoVehiculo() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese su correo electrónico: ");
+        String correo = scanner.nextLine();
+        System.out.println("Ingrese su clave: ");
+        String clave = scanner.nextLine();
+
+        if (!validarCredenciales(correo, clave)) {
+            System.out.println("Credenciales inválidas.");
+            return;
+        }
+
+        System.out.println("Ingrese la información del nuevo vehículo:");
+        System.out.println("Placa: ");
+        String placa = scanner.nextLine();
+        System.out.println("Marca ");
+        String marca = scanner.nextLine();
+        System.out.println("Modelo");
+        String modelo = scanner.nextLine();
+        System.out.println("Tipo de Motor ");
+        String tipoMotor = scanner.nextLine();
+        System.out.println("Año: ");
+        System.out.println
+       
+        Vehiculo nuevoVehiculo = new Vehiculo(/* datos del vehículo */);
+        vehiculos.add(nuevoVehiculo);
+
+        System.out.println("Vehículo ingresado exitosamente.");
+    }
+}
+
