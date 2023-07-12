b7492aaec6892f4938b26f4db960cf46720dfaab
Niariasve <138833714+Niariasve@users.noreply.github.com>
2023-07-11 22:51:27 -0500

    Update Comprador.java

--- proyecto1P/src/main/java/com/pooespol/proyecto1p/Comprador.java
+++ proyecto1P/src/main/java/com/pooespol/proyecto1p/Comprador.java
@@ -1,1 +1,24 @@
+package com.pooespol.proyecto1p;
 
+import java.util.ArrayList;
+
+public class Comprador {
+    private Arraylist<Oferta> ofertas;
+
+    public Comprador(int id, , String nombres, String apellidos, String correo, String organizacion, String clave, ArrayList<Oferta> ofertas) {
+        super(id, nombres, apellidos, correo, organizacion, clave);
+        this.ofertas = ofertas;
+    }
+
+    public ArrayList<Oferta> getOfertas() {
+        return this.ofertas;
+    }
+
+    public void setOfertas(ArrayList<Oferta> ofertas) {
+        this.ofertas = ofertas;
+    }
+
+    public boolean validarUsuario() {
+        
+    }
+}
