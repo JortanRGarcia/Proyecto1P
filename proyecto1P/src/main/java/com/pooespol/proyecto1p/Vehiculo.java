d103adc9f2bf89eccf455d07a576576203e75dff
Niariasve <138833714+Niariasve@users.noreply.github.com>
2023-07-11 21:54:53 -0500

    Update Vehiculo.java

--- proyecto1P/src/main/java/com/pooespol/proyecto1p/Vehiculo.java
+++ proyecto1P/src/main/java/com/pooespol/proyecto1p/Vehiculo.java
@@ -1,3 +1,7 @@
+package com.pooespol;
+
+import java.util.ArrayList;
+
 public class Vehiculo {
   protected int id;
   protected int idVendedor;
@@ -30,6 +34,110 @@ public class Vehiculo {
   }
 
   public Vehiculo(int id, int idVendedor, Vendedor vendedor, String placa, String marca, String modelo, String tipoDeMotor, String anio, double recorrido, String color, String tipoDeCombustible, double precio) {
-    
+    this(id, idVendedor, new ArrayList<Oferta>(), vendedor, placa, marca, modelo, tipoDeMotor, anio, recorrido, color, tipoDeCombustible, precio);
+  }
+
+  public int getId() {
+      return id;
+  }
+
+  public void setId(int id) {
+      this.id = id;
+  }
+
+  public int getIdVendedor() {
+      return idVendedor;
+  }
+
+  public void setIdVendedor(int idVendedor) {
+      this.idVendedor = idVendedor;
+  }
+
+  public ArrayList<Oferta> getOfertas() {
+      return ofertas;
+  }
+
+  public void setOfertas(ArrayList<Oferta> ofertas) {
+      this.ofertas = ofertas;
+  }
+
+  public Vendedor getVendedor() {
+      return vendedor;
+  }
+
+  public void setVendedor(Vendedor vendedor) {
+      this.vendedor = vendedor;
+  }
+
+  public String getPlaca() {
+      return placa;
+  }
+
+  public void setPlaca(String placa) {
+      this.placa = placa;
+  }
+
+  public String getMarca() {
+      return marca;
+  }
+
+  public void setMarca(String marca) {
+      this.marca = marca;
+  }
+
+  public String getModelo() {
+      return modelo;
+  }
+
+  public void setModelo(String modelo) {
+      this.modelo = modelo;
+  }
+
+  public String getTipoDeMotor() {
+      return tipoDeMotor;
+  }
+
+  public void setTipoDeMotor(String tipoDeMotor) {
+      this.tipoDeMotor = tipoDeMotor;
+  }
+
+  public String getAno() {
+      return ano;
+  }
+
+  public void setAno(String ano) {
+      this.ano = ano;
+  }
+
+  public double getRecorrido() {
+      return recorrido;
+  }
+
+  public void setRecorrido(double recorrido) {
+      this.recorrido = recorrido;
+  }
+
+  public String getColor() {
+      return color;
+  }
+
+  public void setColor(String color) {
+      this.color = color;
+  }
+
+  public String getTipoDeCombustible() {
+      return tipoDeCombustible;
+  }
+
+  public void setTipoDeCombustible(String tipoDeCombustible) {
+      this.tipoDeCombustible = tipoDeCombustible;
+  }
+
+  public double getPrecio() {
+      return precio;
+  }
+
+  public void setPrecio(double precio) {
+      this.precio = precio;
   }
 }
