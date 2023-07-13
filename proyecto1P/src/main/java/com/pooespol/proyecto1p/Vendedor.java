f5de0946dd5c6012b16c724524a5515a1ddbdc40
Kcmoranj <138317440+Kcmoranj@users.noreply.github.com>
2023-07-12 23:24:46 -0500

    Update Vendedor.java

--- proyecto1P/src/main/java/com/pooespol/proyecto1p/Vendedor.java
+++ proyecto1P/src/main/java/com/pooespol/proyecto1p/Vendedor.java
@@ -23,39 +23,6 @@ public class Vendedor extends Usuario {
         int nuevoId = obtenerNuevoId();
         return new Vendedor(nuevoId, nombres, apellidos, correo, clave, organizacion);
     }
-
-
-    public void ingresarNuevoVehiculo() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Ingrese su correo electrónico: ");
-        String correo = scanner.nextLine();
-        System.out.println("Ingrese su clave: ");
-        String clave = scanner.nextLine();
-
-        if (!super.validarUsuario(correo, clave)) {
-            System.out.println("Credenciales inválidas.");
-            return;
-        }
-
-        System.out.println("Ingrese la información del nuevo vehículo:");
-        System.out.println("Placa: ");
-        String placa = scanner.nextLine();
-        System.out.println("Marca ");
-        String marca = scanner.nextLine();
-        System.out.println("Modelo");
-        String modelo = scanner.nextLine();
-        System.out.println("Tipo de Motor ");
-        String tipoMotor = scanner.nextLine();
-        System.out.println("Año: ");
-        System.out.println
-       
-        Vehiculo nuevoVehiculo = new Vehiculo(/* datos del vehículo */);
-        vehiculos.add(nuevoVehiculo);
-
-        System.out.println("Vehículo ingresado exitosamente.");
-    }
-
     private static int cargarUltimoId() {
         File file = new File( "vendedor.txt");
         if (!file.exists()) {
@@ -112,11 +79,11 @@ public class Vendedor extends Usuario {
 
             System.out.println("Año: ");
             int año = scanner.nextInt();
-            scanner.nextLine(); // Limpiar el buffer del scanner
+            scanner.nextLine(); 
 
             System.out.println("Recorrido: ");
             int recorrido = scanner.nextInt();
-            scanner.nextLine(); // Limpiar el buffer del scanner
+            scanner.nextLine(); 
 
             System.out.println("Color: ");
             String color = scanner.nextLine();
@@ -131,7 +98,7 @@ public class Vendedor extends Usuario {
             if (tipoVehiculo.equalsIgnoreCase("auto") || tipoVehiculo.equalsIgnoreCase("camioneta")) {
                 System.out.println("Vidrios: ");
                 vidrios = scanner.nextInt();
-                scanner.nextLine(); // Limpiar el buffer del scanner
+                scanner.nextLine(); 
 
                 System.out.println("Transmisión: ");
                 transmision = scanner.nextLine();
@@ -144,13 +111,12 @@ public class Vendedor extends Usuario {
 
             System.out.println("Precio: ");
             double precio = scanner.nextDouble();
-            scanner.nextLine(); // Limpiar el buffer del scanner
+            scanner.nextLine(); 
 
             // Crear el objeto Vehiculo con los datos ingresados
             Vehiculo vehiculo = new Vehiculo(placa, marca, modelo, tipoMotor, año, recorrido, color, tipoCombustible,
                     vidrios, transmision, traccion, precio);
 
-            // Guardar el vehículo en el archivo
             guardarVehiculo(vehiculo);
 
             System.out.println("El vehículo ha sido ingresado correctamente.");
@@ -160,15 +126,14 @@ public class Vendedor extends Usuario {
     }
 
     private boolean validarUsuario(String correo, String clave) {
-        // Validar el usuario y la clave
-        // Implementa aquí tu lógica de validación de usuario y clave
-        return true; // Solo como ejemplo, siempre retorna true en esta implementación
-    }
+        
+        
+        return true; 
 
     private boolean existePlaca(String placa) {
         // Verificar si la placa ya existe en el sistema
         // Implementa aquí tu lógica de verificación de existencia de placa
-        return false; // Solo como ejemplo, siempre retorna false en esta implementación
+
     }
 
     private void guardarVehiculo(Vehiculo vehiculo) {
