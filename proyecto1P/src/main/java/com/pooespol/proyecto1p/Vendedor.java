63ea35ee94810dcb06b1f5446525c76d8d023225
Kcmoranj <138317440+Kcmoranj@users.noreply.github.com>
2023-07-13 00:17:14 -0500

    Update Vendedor.java

--- proyecto1P/src/main/java/com/pooespol/proyecto1p/Vendedor.java
+++ proyecto1P/src/main/java/com/pooespol/proyecto1p/Vendedor.java
@@ -1,8 +1,13 @@
-import java.util.Scanner;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Vendedor extends Usuario {
     private List<Vehiculo> vehiculos;
 
@@ -19,34 +24,25 @@ public class Vendedor extends Usuario {
         this.vehiculos = vehiculos;
     }
 
-   @Override
+    public void guardarVendedor() {
+        File archivo = new File("vendedor.txt");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(archivo, true))) {
+            String vendedorInfo = id + "," + nombres + "," + apellidos + "," + correo + "," + clave;
+            writer.write(vendedorInfo);
+            writer.newLine();
+        } catch (IOException e) {
+            System.out.println("¡Error al guardar el vendedor en el archivo!");
+        }
+    }
+
+    @Override
     protected Usuario crearUsuario(String nombres, String apellidos, String correo, String clave, String organizacion) {
         int nuevoId = obtenerNuevoId();
-        return new Vendedor(nuevoId, nombres, apellidos, correo, clave, organizacion);
+        Vendedor nuevoVendedor = new Vendedor(nuevoId, nombres, apellidos, correo, clave, organizacion);
+        nuevoVendedor.guardarVendedor();
+        return nuevoVendedor;
     }
-    private static int cargarUltimoId() {
-        File file = new File( "vendedor.txt");
-        if (!file.exists()) {
-            return 0;
-        }
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String lastLine = null;
-            String line;
-            while ((line = reader.readLine()) != null) {
-                lastLine = line;
-            }
-
-            if (lastLine != null) {
-                String[] values = lastLine.split(",");
-                return Integer.parseInt(values[0]);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return 0;
-    }
     public void ingresarNuevoVehiculo() {
         Scanner scanner = new Scanner(System.in);
 
@@ -80,11 +76,11 @@ public class Vendedor extends Usuario {
 
             System.out.println("Año: ");
             int año = scanner.nextInt();
-            scanner.nextLine(); 
+            scanner.nextLine();
 
             System.out.println("Recorrido: ");
             int recorrido = scanner.nextInt();
-            scanner.nextLine(); 
+            scanner.nextLine();
 
             System.out.println("Color: ");
             String color = scanner.nextLine();
@@ -99,7 +95,7 @@ public class Vendedor extends Usuario {
             if (tipoVehiculo.equalsIgnoreCase("auto") || tipoVehiculo.equalsIgnoreCase("camioneta")) {
                 System.out.println("Vidrios: ");
                 vidrios = scanner.nextInt();
-                scanner.nextLine(); 
+                scanner.nextLine();
 
                 System.out.println("Transmisión: ");
                 transmision = scanner.nextLine();
@@ -112,7 +108,7 @@ public class Vendedor extends Usuario {
 
             System.out.println("Precio: ");
             double precio = scanner.nextDouble();
-            scanner.nextLine(); 
+            scanner.nextLine();
             Vehiculo vehiculo = new Vehiculo(placa, marca, modelo, tipoMotor, año, recorrido, color, tipoCombustible,
                     vidrios, transmision, traccion, precio);
 
@@ -124,40 +120,61 @@ public class Vendedor extends Usuario {
         }
     }
 
-    private boolean validarUsuario(String correo, String clave) {
-        
-        
-        return true; 
+    private static int cargarUltimoId() {
+        File file = new File("vendedor.txt");
+        if (!file.exists()) {
+            return 0;
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String lastLine = null;
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lastLine = line;
+            }
+
+            if (lastLine != null) {
+                String[] values = lastLine.split(",");
+                return Integer.parseInt(values[0]);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return 0;
+    }
 
     private boolean existePlaca(String placa) throws IOException {
-     BufferedReader reader = new BufferedReader(new FileReader("vehiculos.txt"));
-     String line;
-     while ((line = reader.readLine()) != null) {
-        String[] values = line.split(",");
-        if (values[5].equals(placa)) {
-            reader.close();
-            return true
+        BufferedReader reader = new BufferedReader(new FileReader("vehiculos.txt"));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] values = line.split(",");
+            if (values[5].equals(placa)) {
+                reader.close();
+                return true;
+            }
         }
+        reader.close();
+        return false;
     }
-    reader.close();
-    return false; 
+
+    @Override
+    public String toString() {
+        return id + "," +
+                idVendedor + "," +
+                ofertas + "," +
+                vendedor + "," +
+                placa + "," +
+                marca + "," +
+                modelo + "," +
+                tipoDeMotor + "," +
+                anio + "," +
+                recorrido + "," +
+                color + "," +
+                tipoDeCombustible + "," +
+                precio;
     }
-@Override
-public String toString() {
-    return id + "," +
-           idVendedor + "," +
-           ofertas + "," +
-           vendedor + "," +
-           placa + "," +
-           marca + "," +
-           modelo + "," +
-           tipoDeMotor + "," +
-           anio + "," +
-           recorrido + "," +
-           color + "," +
-           tipoDeCombustible + "," +
-           precio;
-}
+
     private void guardarVehiculo(Vehiculo vehiculo) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("vehiculos.txt", true))) {
             writer.write(vehiculo.toString());
@@ -166,5 +183,40 @@ public String toString() {
             System.out.println("¡Error al guardar el vehículo en el archivo!");
         }
     }
+
+    public static boolean validarCredenciales() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        File archivo = new File(COMPRADOR_FILE);
+
+        if (archivo.exists()) {
+            System.out.println("Ingrese su dirección de correo: ");
+            String correo = scanner.nextLine();
+            System.out.println("Ingrese su contraseña:");
+            String clave = scanner.nextLine();
+
+            BufferedReader reader = new BufferedReader(new FileReader(archivo));
+            String linea;
+            boolean success = false;
+
+            while ((linea = reader.readLine()) != null) {
+                String[] partes = linea.split(",");
+                if (partes[3].equals(correo) && partes[5].equals(Utilitaria.convertirHash(clave))) {
+                    System.out.println("Inicio de sesión exitoso");
+                    success = true;
+                    break;
+                }
+            }
+
+            reader.close();
+
+            if (success) {
+                return true;
+            }
+        }
+
+        System.out.println("Correo o contraseña incorrectos");
+        return false;
+    }
 }
 
+    
