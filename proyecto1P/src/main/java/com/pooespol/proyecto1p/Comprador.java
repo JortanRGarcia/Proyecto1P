dd78e892ae23c2ec641bbfdf22f10eec5c17890e
Niariasve <138833714+Niariasve@users.noreply.github.com>
2023-07-16 12:26:51 -0500

    Update Comprador.java

--- proyecto1P/src/main/java/com/pooespol/proyecto1p/Comprador.java
+++ proyecto1P/src/main/java/com/pooespol/proyecto1p/Comprador.java
@@ -1,29 +1,26 @@
 package com.pooespol.proyecto1p;
-
-import java.io.BufferedReader;
+/**
+ *
+ * @author User
+ */
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author User
- */
 public class Comprador extends Usuario{       
     private static final String COMPRADOR_FILE = "compradores.txt";
     
-    private static int nextId = cargarUltimoId();
+    private static int nextId = Archivo.cargarUltimoId(COMPRADOR_FILE);
     
     private ArrayList<Oferta> ofertas;
     
     public Comprador(int id, String nombres, String apellidos, String correo, String organizacion, String clave, ArrayList<Oferta> ofertas) {
         super(nextId, nombres, apellidos, correo, organizacion, clave);
         this.ofertas = ofertas;
-        guardarVendedorEnArchivo();
+        guardarCompradorEnArchivo();
     }
     
     public Comprador(int id, String nombres, String apellidos, String correo, String organizacion, String clave) {
@@ -40,85 +37,26 @@ public class Comprador extends Usuario{
        
     
     public static void registrar(String[] s) {
-        if(correoYaExiste(s[2])) {
+        if(Archivo.correoYaExiste(COMPRADOR_FILE, s[2])) {
             System.out.println("El correo ya se encuentra en uso.");
         } else {
             Comprador comprador = new Comprador(nextId, s[0], s[1], s[2], s[3], s[4]);
         }       
     }
-       
-    private static boolean correoYaExiste(String correo) {
-        File archivo = new File(COMPRADOR_FILE);
-        if(!archivo.exists()) {
-            return false;
-        }       
-        try (BufferedReader reader = new BufferedReader(new FileReader(archivo))) {
-            String linea;
-            while((linea = reader.readLine()) != null) {
-                String[] partes = linea.split(",");
-                if (partes[3].equals(correo)) {
-                    return true;
-                }
-            }             
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-    
-    public static boolean validarCredenciales() {
+              
+    public static int validarCredenciales() {
         Scanner sc = new Scanner(System.in);
-        File archivo = new File(COMPRADOR_FILE);
-        
-        if (archivo.exists()) {
-            System.out.println("Ingrese su dirección de correo: ");
-            String correo = sc.nextLine();
-            System.out.println("Ingrese su contraseña:");
-            String clave = sc.nextLine();
-            try (BufferedReader reader = new BufferedReader(new FileReader(archivo))) {
-                String linea;
-                while((linea = reader.readLine()) != null) {
-                    String[] partes = linea.split(",");
-                    if (partes[3].equals(correo)) {
-                        if (Utilitaria.convertirHash(clave).equals(partes[5])){
-                            System.out.println("Inicio de sesion éxitoso");
-                            return true;
-                        } else {
-                            System.out.println("Contraseña Incorrecta");
-                            return false;
-                        }
-                    }                     
-                }
-                System.out.println("No se encuentra el correo registrado.");
-                return false;
+        System.out.println("Ingrese su dirección de correo: ");
+        String correo = sc.nextLine();
+        System.out.println("Ingrese su contraseña:");
+        String clave = sc.nextLine();
+        if (Archivo.validarCredenciales(COMPRADOR_FILE, correo, clave)) {
+            return Archivo.buscarIdUsuario(correo, COMPRADOR_FILE);
+        }
+        return 0;
+    }
                 
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        System.out.println("Ha Ocurrido un error.");               
-        return false;
-    }
-    
-    private static int cargarUltimoId() {
-        File file = new File(COMPRADOR_FILE);
-        if (!file.exists()) {
-            return 0; 
-        }
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String lastLine = null;
-            String line;
-            while ((line = reader.readLine()) != null) {
-                lastLine = line;  
-            }
-            return lastLine != null ? Integer.parseInt(lastLine.split(",")[0]) : 0;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-       
-    
-    private void guardarVendedorEnArchivo() {
+    private void guardarCompradorEnArchivo() {
         File archivo = new File(COMPRADOR_FILE);
         if(!archivo.exists()) {
             try {
@@ -133,6 +71,7 @@ public class Comprador extends Usuario{
             throw new RuntimeException(e);
         }
     }
+       
     
     @Override
     public String toString() {
@@ -144,8 +83,7 @@ public class Comprador extends Usuario{
             } else {
                 s.append(o.getId());
             }            
-        }
-        
+        }       
         return this.id +","+this.nombres+","+this.apellidos+","+this.correo+","+this.organizacion+","+Utilitaria.convertirHash(this.clave)+","+s;
-    }
+    }       
 }
