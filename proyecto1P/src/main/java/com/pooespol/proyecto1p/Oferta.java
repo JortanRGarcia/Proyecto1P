dab230b1d80c78d2eb49179f4f755347890ec793
Niariasve <138833714+Niariasve@users.noreply.github.com>
2023-07-16 12:32:40 -0500

    Update Oferta.java

--- proyecto1P/src/main/java/com/pooespol/proyecto1p/Oferta.java
+++ proyecto1P/src/main/java/com/pooespol/proyecto1p/Oferta.java
@@ -1,24 +1,36 @@
 package com.pooespol.proyecto1p;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
 public class Oferta {
+    private final static String OFERTA_FILE = "ofertas.txt";
+    
+    private static int nextId = Archivo.cargarUltimoId(OFERTA_FILE);
+    
     private int id;
     private int idComprador;
     private double valor;
-    private Comprador comprador;
-    private Vehiculo vehiculo;
+    
     private int idVehiculo;
 
-    public Oferta(int id, int idComprador, double valor, Comprador comprador, Vehiculo vehiculo, int idVehiculo) {
-        this.id = id;
+    public Oferta(int id, int idComprador,int idVehiculo, double valor) {
+        this.id = nextId;
         this.idComprador = idComprador;
-        this.valor = valor;
-        this.comprador = comprador;
-        this.vehiculo = vehiculo;
+        this.valor = valor;        
         this.idVehiculo = idVehiculo;
     }
 
     public int getId() {
-        return this.id;
+        return id;
     }
 
     public void setId(int id) {
@@ -26,7 +38,7 @@ public class Oferta {
     }
 
     public int getIdComprador() {
-        return this.idComprador;
+        return idComprador;
     }
 
     public void setIdComprador(int idComprador) {
@@ -34,35 +46,108 @@ public class Oferta {
     }
 
     public double getValor() {
-        return this.valor;
+        return valor;
     }
 
     public void setValor(double valor) {
         this.valor = valor;
     }
-
-    public Comprador getComprador() {
-        return this.comprador;
-    }
-
-    public void setComprador(Comprador comprador) {
-        this.comprador = comprador;
-    }
-
-    public Vehiculo getVehiculo() {
-        return this.vehiculo;
-    }
-
-    public void setVehiculo(Vehiculo vehiculo) {
-        this.vehiculo = vehiculo;
-    }
-
+       
     public int getIdVehiculo() {
-        return this.idVehiculo;
+        return idVehiculo;
     }
 
     public void setIdVehiculo(int idVehiculo) {
         this.idVehiculo = idVehiculo;
     }
+    
+    public void guardarOfertaEnArchivo() {
+        File archivo = new File(OFERTA_FILE);
+        if (!archivo.exists()) {
+            try {
+                archivo.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        try (PrintWriter writer = new PrintWriter(new FileWriter(OFERTA_FILE, true))) {
+            writer.println(this.formatoOfertaArchivo());
+        } catch(IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public static void realizarOferta(int idVehiculo, int idComprador) {
+        if (idComprador != 0 && idVehiculo != -1) {        
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Precio de la oferta:");
+            String precio = sc.nextLine();
+            Oferta oferta = new Oferta(nextId++, idComprador, idVehiculo, Double.parseDouble(precio));
+            oferta.guardarOfertaEnArchivo();
+        }
+    }
+        
+    private String formatoOfertaArchivo() {
+        return this.id+","+this.idComprador+","+this.idVehiculo+","+this.valor;
+    }
+    
+    private static ArrayList<String[]> filtrarOfertas(String[] s) {
+        ArrayList<String[]> ofertas = Archivo.listaLinea(OFERTA_FILE);
+        ArrayList<String[]> ofertasFiltradas = new ArrayList<>();
+        
+        for (String[] oferta: ofertas) {
+            if (oferta[2].equals(s[0])) {
+                String[] filtro = {oferta[1], oferta[3]};
+                ofertasFiltradas.add(filtro);
+            }
+        }
+        return ofertasFiltradas;
+    }
+    
+    public static int menuOferta() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la placa:");
+        String placa = sc.nextLine();
+        int opcion;
+        int c = 0;
+        
+        String[] s = Vehiculo.buscarVehiculo(placa);
+        
+        ArrayList<String[]> oferta = filtrarOfertas(s);
+        System.out.println(s[1]+" "+s[2]+ " " + s[3] + " Precio: " + s[4]);
+        System.out.println("Se han realizado " + oferta.size() + " ofertas.");
+        do {
+            System.out.println("Oferta #" + (c+1));
+            System.out.println("Correo: " + Archivo.buscarCorreo("compradores.txt", oferta.get(c)[0]));
+            System.out.println("Precio: " + oferta.get(c)[1]);
+            System.out.println("""
+                               1. Siguiente
+                               2. Retroceder
+                               3. Aceptar Oferta
+                               4. Salir""");
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    if(c!=oferta.size()-1){
+                        c++;
+                    } else {
+                        System.out.println("No puede avanzar");
+                    }
+                    break;
+                case 2:
+                    if(c!=0) {
+                        c--;
+                    } else {
+                        System.out.println("No puede retroceder");
+                    }
+                    break;
+                case 3:
+                    return c+1;                   
+                default:
+                    break;
+            }                                           
+        } while (opcion != 4);
+        return -1;
+    }    
 }
 
