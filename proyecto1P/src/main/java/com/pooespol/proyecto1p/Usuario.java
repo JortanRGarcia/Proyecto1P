f756a618d17ed1d789a9b9bcf4dd14cda1596f70
Kcmoranj <138317440+Kcmoranj@users.noreply.github.com>
2023-07-16 20:42:14 -0500

    Update Usuario.java

--- proyecto1P/src/main/java/com/pooespol/proyecto1p/Usuario.java
+++ proyecto1P/src/main/java/com/pooespol/proyecto1p/Usuario.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.pooespol.proyecto1p;
 
-/**
- *
- * @author jorgs
- */
+import java.util.Scanner;
+
+
 public abstract class Usuario {
     protected int id;
     protected String nombres;
