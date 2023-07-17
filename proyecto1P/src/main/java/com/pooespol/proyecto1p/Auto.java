402efd0f5a68e863af96e4c90884d554c57ae2da
Kcmoranj <138317440+Kcmoranj@users.noreply.github.com>
2023-07-16 20:10:26 -0500

    Update Auto.java

--- proyecto1P/src/main/java/com/pooespol/proyecto1p/Auto.java
+++ proyecto1P/src/main/java/com/pooespol/proyecto1p/Auto.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.pooespol.proyecto1p;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author User
- */
+/
 public class Auto extends Vehiculo{
     
     
